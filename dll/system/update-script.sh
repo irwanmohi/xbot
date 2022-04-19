621db6ddd069b62ef00cba0d67450705389b056b
mohd irwan bin mohi <48343630+irwanmohi@users.noreply.github.com>
2022-04-20 07:46:35 +0800

    Update update-script.sh

--- dll/system/update-script.sh
+++ dll/system/update-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /usr/bin/yow "https://raw.githubusercontent.com/ahmednajmudeen/tetbot/main/serv-updater.sh" && chmod +x /usr/bin/yow
+wget -q -O /usr/bin/yow "https://raw.githubusercontent.com/irwanmohi/xbot/main/serv-updater.sh" && chmod +x /usr/bin/yow
 screen -S updss yow
